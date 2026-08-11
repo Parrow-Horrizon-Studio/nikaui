95d882f75c9ad6b5398081eed2d0382f20829052
Rowee13 <roweeapor@gmail.com>
2026-08-12 06:38:29 +0800

    feat(web): features section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Features } from "@/components/landing/features";
 import { Hero } from "@/components/landing/hero";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
